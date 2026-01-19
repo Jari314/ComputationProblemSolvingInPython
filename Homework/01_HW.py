81de262a8eda2b9022b1272c47a69521b0252f9a
Jari314 <jariza2458@floridapoly.edu>
2026-01-19 03:56:03 +0000

    Finished first Python HW assignment

--- Homework/01_HW.py
+++ Homework/01_HW.py
@@ -1,3 +1,5 @@
-name = "Juliana Ariza"
-print(name)
-print(name[0])
+my_name = "Juliana Ariza"
+print(my_name)
+print("Use 4 spaces per indentation level.")
+print("The for statement is used to iterate over the elements of a sequence (such as a string, tuple or list) or other iterable object:")
+print("I hope to learn some level of proficiency with python, to improve my programming skills, and to carry the skills I learned into a career")
